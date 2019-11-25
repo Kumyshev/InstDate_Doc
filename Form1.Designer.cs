d200f53ce3369cd636f92d54134584d70a39a232
Kumyshev <darklordsithahmed@gmail.com>
2019-11-25 21:07:36 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace db
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDocMake = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(591, 343);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 60);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(67, 91);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(642, 232);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView_EditingControlShowing);
+            this.dataGridView.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_RowHeaderMouseClick);
+            this.dataGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView_UserDeletingRow);
+            // 
+            // btnDocMake
+            // 
+            this.btnDocMake.Location = new System.Drawing.Point(67, 343);
+            this.btnDocMake.Name = "btnDocMake";
+            this.btnDocMake.Size = new System.Drawing.Size(118, 60);
+            this.btnDocMake.TabIndex = 2;
+            this.btnDocMake.Text = "DocMake";
+            this.btnDocMake.UseVisualStyleBackColor = true;
+            this.btnDocMake.Click += new System.EventHandler(this.btnDocMake_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDocMake);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnDocMake;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace db
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        string path = Path.GetFullPath(@"template.docx");
+        string name;
+        string age;
+        string date;
+
+
+        
+
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\myPROJECTS\+MURAT\demo\db\Database.mdf;Integrated Security=True");
+        SqlDataAdapter adapt;
+        SqlCommand cmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayData();
+
+            
+            
+
+        }
+
+
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("SELECT * FROM [Table]", con);
+
+           
+            adapt.Fill(dt);
+            dataGridView.DataSource = dt;
+            con.Close();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView.CurrentRow != null)
+
+            {
+                DataGridViewRow dgvRow = dataGridView.CurrentRow;
+                cmd = new SqlCommand("INSERT INTO [Table] (Name,Age,Date) VALUES (@Name,@Age,@Date)", con);
+                cmd.Parameters.AddWithValue("@Name", dgvRow.Cells["Name"].Value == DBNull.Value ? "" : dgvRow.Cells["Name"].Value.ToString());
+                cmd.Parameters.AddWithValue("@Age", dgvRow.Cells["Age"].Value == DBNull.Value ? "" : dgvRow.Cells["Age"].Value.ToString());
+                cmd.Parameters.AddWithValue("@Date", dgvRow.Cells["Date"].Value == DBNull.Value ? "" : dgvRow.Cells["Date"].Value.ToString());
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                DisplayData();
+
+            }
+        }
+
+        private void dataGridView_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            if (dataGridView.CurrentCell.ColumnIndex == 5)
+            {
+                e.Control.KeyPress -= AllowNumbersOnly;
+                e.Control.KeyPress += AllowNumbersOnly;
+            }
+
+        }
+        private void AllowNumbersOnly(Object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }              
+        }
+
+        private void dataGridView_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            if (dataGridView.CurrentRow.Cells["id"].Value != DBNull.Value)
+            {
+                cmd = new SqlCommand("DELETE [Table] WHERE id=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(dataGridView.CurrentRow.Cells["id"].Value));
+                cmd.ExecuteNonQuery();
+                con.Close();         
+            }           
+        }
+
+        private void btnDocMake_Click(object sender, EventArgs e)
+        {
+            string currentPath = Directory.GetParent(path).FullName;
+            
+            Word.Application WordApp;
+            Word.Document WordDoc;
+            WordApp = new Word.Application();
+            WordDoc = WordApp.Documents.Open(path);
+
+            ReplaceWord("{name}", name, WordDoc);
+            ReplaceWord("{age}", age, WordDoc);
+            ReplaceWord("{date}", date, WordDoc);
+            WordDoc.SaveAs(currentPath + @"/result.docx");
+            WordApp.Visible = true;
+
+        }
+        private void ReplaceWord(string toReplace, string text, Word.Document WordDoc)
+        {
+            var range = WordDoc.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: toReplace, ReplaceWith: text);
+        }
+
+
+        private void dataGridView_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            name = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            age = dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            date = dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+    }
+}
